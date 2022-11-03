b1cba8fc3446ed478d337fb1bb88040697d832fd
ronantremoureux <ronan.tremoureux@gmail.com>
2022-11-03 15:18:22 +0100

    Changing version of the lib

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'A simple 2D game engine'
 LONG_DESCRIPTION = 'A simple 2D game engine working as unity but in python'
 
