6edec1234c10d02f95d45c3fa4af20f54a338a7e
ronantremoureux <ronan.tremoureux@gmail.com>
2022-12-20 21:13:46 +0100

    adding a bad collision system but the architecture works

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.6'
+VERSION = '0.0.9'
 DESCRIPTION = 'A simple 2D game engine'
 LONG_DESCRIPTION = 'A simple 2D game engine working as unity but in python'
 
@@ -10,14 +10,14 @@ LONG_DESCRIPTION = 'A simple 2D game engine working as unity but in python'
 setup(
     name="PyGine",
     version=VERSION,
-    author="PyGIne (Ronan Tremoureux)",
+    author="PyGIne (Ronan.T)",
     author_email="<ronan.tremoureux@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['pygame'],
-    keywords=['python', 'game', 'engine', 'moteur', 'jeu', 'GameEngine','PyGine','PyGineGame'],
+    keywords=['python', 'game', 'engine', 'moteur', 'jeu', 'GameEngine','PyGine','PyGineGame',"pygame","componentGame","component","gameEngine","game","engine","2D","2DGame","2DGameEngine","2DGameEnginePython","2DGameEnginePygame","2DGameEnginePyGine","PyGineGameEngine","PyGineGameEnginePython","PyGineGameEnginePygame","PyGineGameEnginePyGine"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
